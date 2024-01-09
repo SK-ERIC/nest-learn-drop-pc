14f3a028bfe064bf7fbf44df16bfc24191b2d101
SK-ERIC <974264052@qq.com>
2024-01-09 23:04:08 +0800

    login

---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,104 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginFormPage, ProFormText } from '@ant-design/pro-components';
+import { useMutation } from '@apollo/client';
+import { Tabs, message, theme } from 'antd';
+
+import { LOGIN } from '../../graphql/auth';
+import styles from './index.module.less';
+
+interface IValue {
+  username: string;
+  password: string;
+}
+
+export default () => {
+  const { token } = theme.useToken();
+  const [login] = useMutation(LOGIN);
+
+  const loginHandler = async (values: IValue) => {
+    const res = await login({
+      variables: values,
+    });
+
+    if (res.data.login.code === 200) {
+      message.success(res.data.login.message);
+      return;
+    }
+    message.error(res.data.login.message);
+  };
+
+  const statusRender = (value?: string) => {
+    const getStatus = () => {
+      if (value && value.length > 12) {
+        return 'ok';
+      }
+      if (value && value.length > 6) {
+        return 'pass';
+      }
+      return 'poor';
+    };
+    const status = getStatus();
+    if (status === 'pass') {
+      return <div style={{ color: token.colorWarning }}>强度：中</div>;
+    }
+    if (status === 'ok') {
+      return <div style={{ color: token.colorSuccess }}>强度：强</div>;
+    }
+    return <div style={{ color: token.colorError }}>强度：弱</div>;
+  };
+
+  return (
+    <div className={styles.container}>
+      <LoginFormPage<IValue>
+        initialValues={{ username: 'admin', password: 'admin123' }}
+        backgroundImageUrl="https://mdn.alipayobjects.com/huamei_gcee1x/afts/img/A*y0ZTS6WLwvgAAAAAAAAAAAAADml6AQ/fmt.webp"
+        backgroundVideoUrl="https://gw.alipayobjects.com/v/huamei_gcee1x/afts/video/jXRBRK_VAwoAAAAAAAAAAAAAK4eUAQBr"
+        title="Github"
+        subTitle="全球最大的代码托管平台"
+        onFinish={loginHandler}
+      >
+        <Tabs
+          centered
+          items={[
+            {
+              key: 'account',
+              label: '账号密码登录',
+            },
+          ]}
+        />
+        <>
+          <ProFormText
+            name="username"
+            fieldProps={{
+              size: 'large',
+              prefix: <UserOutlined className="prefixIcon" />,
+            }}
+            placeholder="用户名: admin"
+            rules={[
+              {
+                required: true,
+                message: '请输入用户名!',
+              },
+            ]}
+          />
+          <ProFormText.Password
+            name="password"
+            fieldProps={{
+              size: 'large',
+              prefix: <LockOutlined className="prefixIcon" />,
+              strengthText: '密码应包含数字、字母和特殊字符，长度至少为8个字符。',
+              statusRender,
+            }}
+            placeholder="密码: admin123"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码！',
+              },
+            ]}
+          />
+        </>
+      </LoginFormPage>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import { ApolloProvider } from '@apollo/client';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
+
+import App from './App';
 import './index.css';
+import { client } from './utils/apollo';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
+  </ApolloProvider>,
 );
